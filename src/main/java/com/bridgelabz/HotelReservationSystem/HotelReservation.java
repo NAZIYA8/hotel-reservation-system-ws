aac170846dbb4274f372c8361e5317a0e9a15c30
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-26 09:33:22 +0530

    Welcome to Hotel Reservation System

---
+++ src/main/java/com/bridgelabz/HotelReservationSystem/HotelReservation.java
@@ -0,0 +1,19 @@
+/**
+ * ****************************************************************************
+ * Purpose: This class is Hotel Reservation class which contains different methods
+ * that can be called.it also contains the main method.
+ *
+ * @author Syeda Naziya
+ * @version 1.0
+ * @since 26-06-2021
+ * ****************************************************************************
+ */
+
+package com.bridgelabz.HotelReservationSystem;
+
+public class HotelReservation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation System");
+	}
+}
